0dc241bab36938d102446708c11f274358c35227
guolongzhang <guolongzhang@ebnew.com>
2017-11-26 14:55:05 +0800

    init

---
+++ gof-algorithm/src/main/java/cn/zrpo/gof/algorithm/sort/BubbleSort.java
@@ -0,0 +1,24 @@
+package cn.zrpo.gof.algorithm.sort;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 冒泡排序
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] nums = {1,23,44,7,3,65,2};
+        new BubbleSort(nums);
+    }
+
+    BubbleSort(int[] arrays){
+
+        for (int i = 0 ; i<arrays.length;i++){
+
+
+        }
+    }
+
+}
